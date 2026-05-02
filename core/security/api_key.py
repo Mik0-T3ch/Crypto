4c3c6ac32bd6afcca074b60ddbbe08f1b7d5bcc6
Mik0-T3ch <chrispino2109@gmail.com>
2026-05-01 22:26:20 -0600

    api key

--- core/security/api_key.py
+++ core/security/api_key.py
@@ -1,7 +1,5 @@
 import os
 
-VALID_KEYS = os.getenv("VAULT_API_KEYS", "").split(",")
-
-def verify_api_key(key: str):
-    if key not in VALID_KEYS:
-        raise Exception("Unauthorized")
+def verify_api_key(key: str) -> bool:
+    expected = os.getenv("API_KEY")
+    return key == expected
